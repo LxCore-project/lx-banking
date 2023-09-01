4b938449e7bdf6f36a15e3ac616150b099f31eca
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-09-01 13:23:32 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,2 +1,3 @@
 local  lxCore = exports["lxCore"]:SharedObject()
 local getplayer = LxCore.GetPlayers()
+---- if you pull https://github.com/LxCore-project/lxCore/pull/6/files      then use local playerData = exports['lxCore']:getplayer()
